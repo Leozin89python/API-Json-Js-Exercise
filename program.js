853e9f177a9eba94d93c64b4b5cd2926ff6e7ab0
Leozin89Python <leozin069@gmail.com>
2020-12-06 18:45:48 -0300

    API V1.1.1

---
+++ program.js
@@ -0,0 +1,5 @@
+const { json } = require('express')
+const db = require('./database.json')
+//valor = JSON.parse(db) converte objeto ou string em string JSON
+valor = JSON.stringify(db) //converte string JSON em objeto javascript
+console.log(valor)
