dce270d7c7a9e3b500ef9cdc0e017473e359ed4a
Leozin89python <62114258+Leozin89python@users.noreply.github.com>
2020-12-06 18:55:09 -0300

    Update program.js

--- src/program.js
+++ src/program.js
@@ -1,4 +1,3 @@
-const { json } = require('express')
 const db = require('./database.json')
 //valor = JSON.parse(db) converte objeto ou string em string JSON
 valor = JSON.stringify(db) //converte string JSON em objeto javascript
